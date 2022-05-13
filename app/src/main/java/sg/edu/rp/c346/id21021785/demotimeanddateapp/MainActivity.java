62b0d144b03d9139abbd2449e1abd6ef2d9786fe
21021785 <21021785@myrp.edu.sg>
2022-05-13 12:36:12 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21021785/demotimeanddateapp/MainActivity.java
@@ -0,0 +1,57 @@
+package sg.edu.rp.c346.id21021785.demotimeanddateapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DatePicker dp;
+        TimePicker tp;
+        Button btnDisplayDate;
+        Button btnDisplayTime;
+        TextView tvDisplay;
+        Button btnReset;
+
+        dp = findViewById(R.id.datePicker);
+        tp = findViewById(R.id.timePicker);
+        btnDisplayDate = findViewById(R.id.buttonDisplayDate);
+        btnDisplayTime = findViewById(R.id.buttonDisplayTime);
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnReset = findViewById(R.id.btnReset);
+
+        btnDisplayTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvDisplay.setText("Time is " + tp.getCurrentHour() + ":" + tp.getCurrentMinute());
+            }
+        });
+
+        btnDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int month = (Integer.parseInt(String.valueOf(dp.getMonth()))+ 1);
+                tvDisplay.setText("Date is " + dp.getDayOfMonth() + "/" + month + "/" + dp.getYear());
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tp.setCurrentHour(0);
+                tp.setCurrentMinute(0);
+                dp.updateDate(2020, 0, 1);
+            }
+        });
+    }
+}
